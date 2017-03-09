9d360e44dcd439a772c52991b07c5d43fff67199
alexnemankov <alexander1492@mail.ru>
2017-03-10 01:30:44 +0300

    source files
    added

---
+++ app/js/common.js
@@ -0,0 +1,21 @@
+$(function() {
+
+	// Custom JS
+$('.single-item').slick({
+//    autoplay: true,
+//  autoplaySpeed: 7500,
+dots: true,
+});
+    
+});
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(500);
+  });//end slide toggle
+  
+  $(window).resize(function() {		
+		if (  $(window).width() > 1280 ) {			
+			$('nav ul').removeAttr('style');
+		 }
+	});//end resize
+});//end ready
